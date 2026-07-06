089d78ec357cb82f23491966622c488195b8c220
Sebdri <sebdri@outlook.com>
2026-07-06 16:13:26 -0600

    Uploading first exercises

---
+++ Python basico/Diccionarios/Ejemplo 1 y 2.py
@@ -0,0 +1,13 @@
+hotel = {
+    "name": "Hotel Paradise",
+    "number_of_stars": 5,
+    "rooms": [
+        {
+            "Number": 115,
+            "Floor": 3,
+            "price_per_night": 50,
+        }
+    ]
+}
+
+print (hotel)
